4b74d6aa5de8a48ba938c4f5a65bd700e19cb0fe
GlimmerDev <leifericson96@gmail.com>
2024-03-25 21:11:39 -0400

    initial version

---
+++ SleepWarning/ModEntry.cs
@@ -0,0 +1,65 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace SleepWarning
+{
+    /// <summary>The mod entry point.</summary>
+    internal class ModEntry : Mod
+    {
+        /*********
+        ** Properties
+        *********/
+        /// <summary>The mod configuration from the player.</summary>
+        private SleepWarningConfig Config;
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            this.Config = Helper.ReadConfig<SleepWarningConfig>();
+            helper.Events.GameLoop.TimeChanged += this.OnTimeChanged;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>
+        /// Plays the sleep warning sound and repeats it the specified amount of times.
+        /// </summary>
+        /// <param name="repeat">The number of times to repeat.</param>
+        private void PlayWarningSound(int repeat)
+        {
+            for (int i = 0; i < repeat; i++)
+            {
+                DelayedAction.playSoundAfterDelay(Config.WarningSound, 500*i, null, null, 1, true);
+            }
+        }
+
+        /// <inheritdoc cref="IGameLoopEvents.TimeChanged"/>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnTimeChanged(object? sender, TimeChangedEventArgs e) 
+        {
+            if (Game1.timeOfDay < Config.FirstWarnTime)
+                return;
+
+            if (Config.ThirdWarnTime > -1 && Game1.timeOfDay == Config.ThirdWarnTime)
+            {
+                PlayWarningSound(3);
+            }
+            else if (Config.SecondWarnTime > -1 && Game1.timeOfDay == Config.SecondWarnTime)
+            {
+                PlayWarningSound(2);
+            }
+            else if (Config.FirstWarnTime > -1 && Game1.timeOfDay == Config.FirstWarnTime)
+            {
+                PlayWarningSound(1);
+            }
+        }
+    }
+}
---
+++ SleepWarning/SleepWarningConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepWarning
+{
+    internal class SleepWarningConfig
+    {
+        public int FirstWarnTime { get; set; } = 2300;
+        public int SecondWarnTime { get; set; } = 2400;
+        public int ThirdWarnTime { get; set; } = 2500;
+        public string WarningSound = "crystal";
+    }
+}
